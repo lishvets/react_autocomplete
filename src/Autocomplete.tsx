c2f8f7d9802cd1993f7fc4e6719cb384987ac1ec
Lidiia Shvets <lepoki051003@gmail.com>
2026-03-31 18:17:36 +0200

    react_autocomplete

---
+++ src/Autocomplete.tsx
@@ -0,0 +1,94 @@
+import React, { useState, useEffect } from 'react';
+import { Person } from './types/Person';
+import debounce from 'lodash.debounce';
+
+type AutocompleteProps = {
+  people: Person[];
+  onSelected: (person: Person | null) => void;
+  delay?: number;
+};
+
+export const Autocomplete: React.FC<AutocompleteProps> = ({
+  people,
+  onSelected,
+  delay = 300,
+}) => {
+  const [query, setQuery] = useState('');
+  const [suggestions, setSuggestions] = useState<Person[]>([]);
+  const [isOpen, setIsOpen] = useState(false);
+  const handleQueryChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setQuery(event.target.value);
+    onSelected(null);
+  };
+
+  useEffect(() => {
+    const handler = debounce((input: string) => {
+      if (!input) {
+        setSuggestions(people);
+      } else {
+        const filtered = people.filter(person =>
+          person.name.toLowerCase().includes(input.toLowerCase()),
+        );
+
+        setSuggestions(filtered);
+      }
+    }, delay);
+
+    handler(query);
+
+    return () => {
+      handler.cancel();
+    };
+  }, [query, people, delay]);
+
+  return (
+    <div className={`dropdown ${isOpen ? 'is-active' : ''}`}>
+      <div className="dropdown-trigger">
+        <input
+          type="text"
+          placeholder="Enter a part of the name"
+          className="input"
+          data-cy="search-input"
+          value={query}
+          onChange={handleQueryChange}
+          onFocus={() => setIsOpen(true)}
+        />
+      </div>
+
+      <div className="dropdown-menu" role="menu" data-cy="suggestions-list">
+        <div className="dropdown-content">
+          {suggestions.map(person => (
+            <div
+              className="dropdown-item"
+              data-cy="suggestion-item"
+              key={person.slug}
+              onClick={() => {
+                setQuery(person.name);
+                setIsOpen(false);
+                onSelected(person);
+              }}
+            >
+              <p
+                className={
+                  person.sex === 'm' ? 'has-text-link' : 'has-text-danger'
+                }
+              >
+                {person.name}
+              </p>
+            </div>
+          ))}
+        </div>
+        {isOpen && suggestions.length === 0 && query && (
+          <div
+            className="notification is-danger is-light mt-3
+            is-align-self-flex-start"
+            role="alert"
+            data-cy="no-suggestions-message"
+          >
+            <p className="has-text-danger">No matching suggestions</p>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
